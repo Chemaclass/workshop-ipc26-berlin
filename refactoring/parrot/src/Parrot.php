bc4e86180b29dbc305bc619b01112006999b69de
sebu91 <sb91@hotmail.de>
2026-06-08 14:23:32 +0200

    moved logic calculation to functions

--- refactoring/parrot/src/Parrot.php
+++ refactoring/parrot/src/Parrot.php
@@ -11,12 +11,12 @@ class Parrot
     private const float MIN_VALUE = 24.0;
     private const float LOAD_FACTOR = 9.0;
     private const float BASE_SPEED = 12.0;
-    
+
 
     private const string SQOORK ='Sqoork!';
-       private const string SQAARK ='Sqaark!';
-       private const string BZZZT = 'Bzzzzzz';
-       private const SILENCE='...';
+    private const string SQAARK ='Sqaark!';
+    private const string BZZZT = 'Bzzzzzz';
+    private const string SILENCE='...';
     public function __construct(
         /**
          * @var int ParrotTypeEnum
@@ -35,10 +35,20 @@ class Parrot
     {
         return match ($this->type) {
             ParrotTypeEnum::EUROPEAN->value => $this->getBaseSpeed(),
-            ParrotTypeEnum::AFRICAN->value => max(0, $this->getBaseSpeed() - $this->getLoadFactor() * $this->numberOfCoconuts),
-            ParrotTypeEnum::NORWEGIAN_BLUE->value => $this->isNailed ? 0 : $this->getBaseSpeedWith($this->voltage),
+            ParrotTypeEnum::AFRICAN->value => $this->getSpeedForAfrican(),
+            ParrotTypeEnum::NORWEGIAN_BLUE->value => $this->getSpeedForNorwegianBlue($this->isNailed),
             default => throw new Exception('Should be unreachable'),
         };
+    }
+
+    private function getSpeedForAfrican(): float
+    {
+        return max(0, $this->getBaseSpeed() - $this->getLoadFactor() * $this->numberOfCoconuts);
+    }
+
+    private function getSpeedForNorwegianBlue(bool $isNailed): float
+    {
+        return $isNailed ? 0 : $this->getBaseSpeedWith($this->voltage);
     }
 
     /**
@@ -49,9 +59,14 @@ class Parrot
         return match ($this->type) {
             ParrotTypeEnum::EUROPEAN->value => self::SQOORK,
             ParrotTypeEnum::AFRICAN->value => self::SQAARK,
-            ParrotTypeEnum::NORWEGIAN_BLUE->value => $this->voltage > 0 ? self::BZZZT : self::SILENCE,
+            ParrotTypeEnum::NORWEGIAN_BLUE->value => $this->getCryForNorwegianBlue(),
             default => throw new Exception('Should be unreachable'),
         };
+    }
+
+    private function getCryForNorwegianBlue(): string
+    {
+        return $this->voltage > 0 ? self::BZZZT : self::SILENCE;
     }
 
     private function getBaseSpeedWith(float $voltage): float
