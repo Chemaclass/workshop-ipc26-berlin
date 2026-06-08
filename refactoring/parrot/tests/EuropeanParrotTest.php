4606e1210c59716386d43c4e8c0cb99adef4b9b9
Roman Frolov <roman.frolov@purpose-green.com>
2026-06-08 14:21:48 +0200

    extract EuropeanParrotTest

---
+++ refactoring/parrot/tests/EuropeanParrotTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ParrotTests;
+
+use Parrot\EuropeanParrot;
+use PHPUnit\Framework\TestCase;
+
+class EuropeanParrotTest extends TestCase
+{
+    public function testSpeedOfEuropeanParrot(): void
+    {
+        $parrot = new EuropeanParrot(0, 0, false);
+        self::assertSame(12.0, $parrot->getSpeed());
+    }
+
+    public function testGetCryOfEuropeanParrot(): void
+    {
+        $parrot = new EuropeanParrot(0, 0, false);
+        self::assertSame('Sqoork!', $parrot->getCry());
+    }
+}
