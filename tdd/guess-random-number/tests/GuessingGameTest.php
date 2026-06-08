ffbbdc8cf3f65fe9169efd6f051f33dd82e120d6
Konstantinos Giavridis <konstantinos.giavridis@cardmarket.com>
2026-06-08 11:39:23 +0200

    first guess tests

--- tdd/guess-random-number/tests/GuessingGameTest.php
+++ tdd/guess-random-number/tests/GuessingGameTest.php
@@ -15,8 +15,37 @@ final class GuessingGameTest extends TestCase
         $mock = $this->createMock(RandomNumberGenerator::class);
         $mock->method('generate')->willReturn(5);
 
-        $changeMe = new GuessingGame($mock);
+        $game = new GuessingGame($mock);
 
-        self::assertSame(5,$changeMe->getSystemNumber());
+        self::assertSame(5,$game->getSystemNumber());
+    }
+
+    public function test_guess_Number_First_Try(): void
+    {
+        $mock = $this->createMock(RandomNumberGenerator::class);
+        $mock
+            ->method('generate')
+            ->willReturn(5);
+
+        $game = new GuessingGame($mock);
+
+        $result = $game->playRound(5);
+
+        self::assertTrue($result);
+
+    }
+   public function test_Guess_Number_Incorrect_First_Try(): void
+    {
+        $mock = $this->createMock(RandomNumberGenerator::class);
+        $mock
+            ->method('generate')
+            ->willReturn(6);
+
+        $game = new GuessingGame($mock);
+
+        $result = $game->playRound(5);
+
+        self::assertFalse($result);
+
     }
 }
